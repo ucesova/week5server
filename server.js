35090be9304b340426816a4db3acec3a6485ed22
ucesova <ucesova@ucl.ac.uk>
2018-02-22 10:54:58 -0500

    using express static routes

--- server.js
+++ server.js
@@ -10,15 +10,5 @@ var credentials = {key: privateKey, cert: certificate};
 var httpsServer = https.createServer(credentials, app);
 httpsServer.listen(4443);
 
-app.get('/', function (req, res) {
- // run some server-side code
-console.log('the server has received a request');
-res.send('Hello World');
-});
-
-app.get('/test.html', function (req, res) {
- // run some server-side code
-console.log('test.html requested');
-// note that __dirname gives the path to the server.js file
-res.sendFile(__dirname + '/test.html');
-});
+// serve static files - e.g. html, css
+app.use(express.static(__dirname));
