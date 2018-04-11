e30d68752d09f816fbf77eadfdcca60f058d6c7b
ucesova <ucesova@ucl.ac.uk>
2018-04-11 16:06:07 -0400

    uploading data from the form

--- httpServer.js
+++ httpServer.js
@@ -175,7 +175,16 @@ app.post('/uploadData',function(req,res){
 		console.log("not able to get connection "+ err);
 		res.status(400).send(err);
 		}
-		var querystring = "INSERT into formdata (name,surname,module) values('" + req.body.name + "','" + req.body.surname + "','" + req.body.module+"')";
+	
+		// pull the geometry component together
+		// note that well known text requires the points as longitude/latitude !
+		// well known text should look like: 'POINT(-71.064544 42.28787)'
+		var geometrystring = "st_geomfromtext('POINT(" + req.body.longitude + " " + req.body.latitude + ")'";
+		
+		var querystring = "INSERT into formdata (name,surname,module,language, modulelist, lecturetime, geom) values ('";
+		querystring = querystring + req.body.name + "','" + req.body.surname + "','" + req.body.module + "','";
+		querystring = querystring + req.body.language + "','" + req.body.modulelist + "','" + req.body.lecturetime+"',"+geometrystring + "))";
+
 		console.log(querystring);
 		client.query( querystring,function(err,result) {
 			done();
