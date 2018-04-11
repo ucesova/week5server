2a9c3be634a6e8500dc6e10b8dd78c1fe2b9b95f
ucesova <ucesova@ucl.ac.uk>
2018-04-11 13:41:50 -0400

    allowing the httpServer to process the uploaded data

--- httpServer.js
+++ httpServer.js
@@ -157,7 +157,21 @@ app.get('/getGeoJSON/:tablename/:geomcolumn', function (req,res) {
 		});
 	});
 });
-	
+
+// adding the body parser so that it would be able to process the uploaded data	
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+app.post('/uploadData',function(req,res){
+	// note that we are using POST here as we are uploading data
+	// so the parameters form part of the BODY of the request rather than the RESTful API
+	console.dir(req.body);
+	// for now, just echo the request back to the client
+	res.send(req.body);
+});
 	
 	// the / indicates the path that you type into the server - in this case, what happens when you type in:  http://developer.cege.ucl.ac.uk:32560/xxxxx/xxxxx
   app.get('/:name1', function (req, res) {
